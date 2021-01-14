2906f078aaa0056fd34b63452821b2531c4689e3
PatrykWalach <patryk_walach@outlook.com>
2021-01-14 16:59:56 +0100

    feat: finish timer

--- projekt.c
+++ projekt.c
@@ -1,23 +1,23 @@
 #include <8051.h>
-#define TIMER_REFRESH_RATE_TL 18
-#define TIMER_REFRESH_RATE_TH 0
-#define TIMER_REFRESH_RATE 200
-#define TIMER_MODE Tmod_13_BIT
-#define timerDisplayDigitsLength 6
+#define TIMER_REFRESH_RATE_TH_0 0b11111101
+#define TIMER_REFRESH_RATE_TL_0 0b11000000
+#define TIMER_REFRESH_RATE_0 1600
+#define TIMER_MODE_0 TimerMode0_16_BIT
 
-enum Tmods {
-  Tmod_13_BIT = 0,
-  Tmod_16_BIT = 1,
-  Tmod_8_BIT = 2,
-  Tmod_2_X_8_BIT = 3
+enum TimerMode0 {
+  TimerMode0_13_BIT = 0,
+  TimerMode0_16_BIT = T0_M0,
+  TimerMode0_8_BIT = T0_M1,
+  TimerMode0_2_X_8_BIT = T0_M0 | T0_M1
 };
 
 enum Time { Time_SECONDS = 0, Time_MINUTES = 1, Time_HOURS = 2 };
 
 volatile unsigned char timerDisplayRefreshed;
+#define timerDisplayDigitsLength 6
 volatile unsigned char timerDisplayDigits[timerDisplayDigitsLength];
 volatile unsigned char timerDisplayEditedSection;
-volatile unsigned char isTimerDisplayEdited;
+__bit isTimerDisplayEdited;
 
 __xdata __at(0x0FF38) volatile unsigned char DISPLAY_DATA;
 __xdata __at(0x0FF30) volatile unsigned char DISPLAY_REFRESHED;
@@ -40,24 +40,10 @@ __code unsigned char timerDisplayDigitToData[] = {
     0x6F,  // ;9
 };
 
-void timerDisplayRefresh() {
-  P1_6 = 1;
+volatile unsigned char multiplexKeysPressed[6];
+volatile unsigned char previousMultiplexKeysPressed[6];
 
-  timerDisplayRefreshed++;
-  if (timerDisplayRefreshed >= timerDisplayDigitsLength) {
-    timerDisplayRefreshed = 0;
-  }
-
-  DISPLAY_REFRESHED = 1 << timerDisplayRefreshed;
-
-  DISPLAY_DATA =
-      timerDisplayDigitToData[timerDisplayDigits[timerDisplayRefreshed]] |
-      ((timerDisplayRefreshed && timerDisplayRefreshed % 2 == 0) << 7);
-
-  P1_6 = 0;
-}
-
-void timerDisplayinit() {
+void initTimerDisplay() {
   unsigned char i;
   P1_6 = 1;
   timerDisplayRefreshed = 0;
@@ -72,16 +58,16 @@ void timerDisplayinit() {
 }
 
 volatile unsigned char hasSecondPassed;
-volatile unsigned char counter;
+volatile unsigned short counter;
 unsigned char time[3];
 
 void initTimer0() {
   unsigned char i = 0;
   ET0 = 1;
   EA = 1;
-  TMOD = TIMER_MODE;
-  TL0 = TIMER_REFRESH_RATE_TL;
-  TH0 = TIMER_REFRESH_RATE_TH;
+  TMOD = TMOD & T1_MASK | TIMER_MODE_0;
+  TL0 = TIMER_REFRESH_RATE_TL_0;
+  TH0 = TIMER_REFRESH_RATE_TH_0;
   TR0 = 1;
   hasSecondPassed = 0;
   counter = 0;
@@ -95,54 +81,37 @@ unsigned char min(unsigned char a, unsigned char b) { return a < b ? a : b; }
 unsigned char max(unsigned char a, unsigned char b) { return a > b ? a : b; }
 
 enum MultiplexKeyboardKey {
-  MultiplexKeyboardKey_ENTER = 1 << 0,
-  MultiplexKeyboardKey_ESC = 1 << 1,
-  MultiplexKeyboardKey_RIGHT = 1 << 2,
-  MultiplexKeyboardKey_UP = 1 << 3,
-  MultiplexKeyboardKey_DOWN = 1 << 4,
-  MultiplexKeyboardKey_LEFT = 1 << 5,
+  MultiplexKeyboardKey_ENTER = 0,
+  MultiplexKeyboardKey_ESC = 1,
+  MultiplexKeyboardKey_RIGHT = 2,
+  MultiplexKeyboardKey_UP = 3,
+  MultiplexKeyboardKey_DOWN = 4,
+  MultiplexKeyboardKey_LEFT = 5,
 };
 
-unsigned char multiplexKeysPressed;
-unsigned char previousMultiplexKeysPressed;
-unsigned char multiplexKeyups;
 unsigned int prevTime[3];
 
 void initMultiplexKeyboard() {
   unsigned char i = 0;
   timerDisplayEditedSection = 0;
-  multiplexKeysPressed = 0;
 
+  for (i = 0; i < 6; i++) {
+    multiplexKeysPressed[i] = 0;
+    previousMultiplexKeysPressed[i] = 0;
+  }
   for (i = 0; i < 3; i++) {
     prevTime[i] = 0;
   }
 }
 
-void readMultiplexKeyboard() {
-  unsigned char key, i;
-  previousMultiplexKeysPressed = multiplexKeysPressed;
-  multiplexKeysPressed = 0;
-  P1_6 = 1;
-
-  for (i = 0; i < timerDisplayDigitsLength; i++) {
-    key = 1 << i;
-    DISPLAY_REFRESHED = key;
-    if (P3_5) {
-      multiplexKeysPressed |= key;
-    } else {
-      multiplexKeysPressed &= ~key;
-    }
-  }
-
-  multiplexKeyups = (previousMultiplexKeysPressed ^ multiplexKeysPressed) &
-                    multiplexKeysPressed;
-
-  DISPLAY_REFRESHED = 1 << timerDisplayRefreshed;
-  P1_6 = 0;
-}
-
 unsigned char isMultiplexKeyup(unsigned char key) {
-  return (multiplexKeyups & key) == key;
+  unsigned char r =
+      multiplexKeysPressed[key] &&
+      (previousMultiplexKeysPressed[key] != multiplexKeysPressed[key]);
+
+  previousMultiplexKeysPressed[key] = multiplexKeysPressed[key];
+
+  return r;
 }
 
 enum MatrixKeyboardKey {
@@ -184,14 +153,13 @@ void timerDisplayUpdateSection(unsigned char nextTimerDisplayEditedSection) {
   updateTime();
   timerDisplayEditedSection = nextTimerDisplayEditedSection;
 
-  timerDisplayDigits[timerDisplayEditedSection * 2] = 10;
-  timerDisplayDigits[timerDisplayEditedSection * 2 + 1] = 10;
+  timerDisplayDigits[timerDisplayEditedSection * 2] = -1;
+  timerDisplayDigits[timerDisplayEditedSection * 2 + 1] = -1;
 
   if (isTimerDisplayEdited) {
     return;
   }
 
-  // prevTime = time;
   for (i = 0; i < 3; i++) {
     prevTime[i] = time[i];
   }
@@ -295,8 +263,8 @@ void LCDcommand(unsigned char command) {
 #define historyLength 7
 __xdata __at(0x4000) volatile char history[historyLength][16];
 
-unsigned char historyIndex;
-unsigned char historyWriteIndex;
+volatile unsigned char historyIndex;
+volatile unsigned char historyWriteIndex;
 
 void writeLCD(unsigned char write) {
   LCDwaitWhileBusy();
@@ -333,12 +301,18 @@ void handleMatrixKeyboard() {
 }
 
 void initLCD() {
+  unsigned char i, j;
   LCDcommand(0b111000);
   LCDcommand(0b1111);
   LCDcommand(0b110);
   LCDcommand(0b1);
 
   historyIndex = 0;
+  for (i = 0; i < historyLength; i++) {
+    for (j = 0; j < 16; j++) {
+      history[i][j] = ' ';
+    }
+  }
   historyWriteIndex = 0;
 }
 
@@ -352,43 +326,192 @@ void historyWrite(char* command) {
   historyWriteIndex %= historyLength;
 }
 
-void main() {
-  initTimer0();
-  timerDisplayinit();
-  initMultiplexKeyboard();
-  initLCD();
+__bit isSending;
+volatile unsigned char receiveBuffer[16];
+volatile unsigned char receiveBufferIndex;
 
-  while (1) {
-    readMultiplexKeyboard();
-    handleMultiplexKeyboard();
+volatile unsigned char sendBuffer[11];
+volatile unsigned char sendBufferIndex;
 
-    readMatrixKeyboard();
-    handleMatrixKeyboard();
+enum Commands {
+  Commands_GET = 0,
+  Commands_EDIT = 1,
+  Commands_SET = 2,
+  Commands_ERROR,
+};
 
-    if (hasSecondPassed) {
-      hasSecondPassed = 0;
-      P1_5 = !P1_5;
-      counter -= TIMER_REFRESH_RATE;
+unsigned char prevCommandInterruptedAt;
+volatile unsigned char commandInterruptedAt;
 
-      if (!isTimerDisplayEdited) {
-        addTime(0, 0, 1);
-      }
-    }
-    timerDisplayRefresh();
+void initCommands() {
+  unsigned char i = 0;
+  ES = 1;
+  EA = 1;
+
+  receiveBufferIndex = 0;
+  for (i = 0; i < 16; i++) {
+    receiveBuffer[i] = ' ';
   }
+  sendBufferIndex = 0;
+  isSending = 0;
+  prevCommandInterruptedAt = 0;
+  commandInterruptedAt = 0;
+
+  SCON = 0x50;
+  // M0 M1 M2 REN TB8 RB8 TI RI
+  TMOD = TMOD & T0_MASK | 0x20;
+  TL1 = 0xFA;
+  TH1 = 0xFA;
+  PCON &= 0x7F;
+  TF1 = 0;
+  TR1 = 1;
 }
 
-void handleTimer0Interrupt(void) __interrupt(1) {
-  TL0 = TIMER_REFRESH_RATE_TL;
-  TH0 = TIMER_REFRESH_RATE_TH;
+void handleTimer0Interrupt(void) __interrupt(TF0_VECTOR) {
+  TL0 |= TIMER_REFRESH_RATE_TL_0;
+  TH0 = TIMER_REFRESH_RATE_TH_0;
+
+  timerDisplayRefreshed++;
+
+  if (timerDisplayRefreshed >= timerDisplayDigitsLength) {
+    timerDisplayRefreshed = 0;
+  }
+
+  P1_6 = 1;
+
+  DISPLAY_REFRESHED = 1 << timerDisplayRefreshed;
+  multiplexKeysPressed[timerDisplayRefreshed] = P3_5;
+  DISPLAY_DATA =
+      timerDisplayDigitToData[timerDisplayDigits[timerDisplayRefreshed]] |
+      ((timerDisplayRefreshed && timerDisplayRefreshed % 2 == 0) << 7);
+
+  P1_6 = 0;
 
   counter++;
-  if (counter < TIMER_REFRESH_RATE) {
+  if (counter < TIMER_REFRESH_RATE_0) {
     return;
   }
   hasSecondPassed = 1;
 }
 
-// historyWrite("1             OK");
+__code volatile unsigned char tests[3][2][13] = {
+    {"GET", "GET"},
+    {"EDIT", "EDIT"},
+    {"SET 00.00.00", "SET 99.99.99"},
+};
+__code volatile unsigned char testsLengths[3] = {3, 4, 12};
 
-void handleSerialPortInterrupt(void) __interrupt(4) {}
+unsigned char isCommand(unsigned char* string, unsigned char command) {
+  unsigned char i;
+  for (i = 0; i < testsLengths[command]; i++) {
+    if ((string[i] < tests[command][0][i]) ||
+        (string[i] > tests[command][1][i])) {
+      return 0;
+    }
+  }
+  return 1;
+}
+
+void handleSerialPortInterrupt(void) __interrupt(SI0_VECTOR) {
+  if (TI) {
+    TI = 0;
+    isSending = 0;
+    return;
+  }
+  commandInterruptedAt++;
+  if (receiveBufferIndex < 12) {
+    receiveBuffer[receiveBufferIndex++] = SBUF;
+  } else {
+    receiveBuffer[10] = '.';
+    receiveBuffer[11] = '.';
+  }
+  RI = 0;
+}
+
+void handleCommands() {
+  unsigned char i;
+  unsigned char ok = 1;
+  if (!receiveBufferIndex || prevCommandInterruptedAt != commandInterruptedAt) {
+    return;
+  }
+
+  if (isCommand(receiveBuffer, Commands_EDIT)) {
+    timerDisplayUpdateSection(timerDisplayEditedSection);
+  } else if (isCommand(receiveBuffer, Commands_SET)) {
+    for (i = 0; i < 3; i++) {
+      time[i] = 0;
+    }
+    addTime((receiveBuffer[4] - '0') * 10 + receiveBuffer[5] - '0',
+            (receiveBuffer[7] - '0') * 10 + receiveBuffer[8] - '0',
+            (receiveBuffer[10] - '0') * 10 + receiveBuffer[11] - '0');
+  } else if (isCommand(receiveBuffer, Commands_GET)) {
+    sendBuffer[sendBufferIndex++] = '\n';
+    sendBuffer[sendBufferIndex++] = '\r';
+    for (i = 0; i < 3; i++) {
+      if (i) {
+        sendBuffer[sendBufferIndex++] = '.';
+      }
+      sendBuffer[sendBufferIndex++] = (time[i] % 10) + '0';
+      sendBuffer[sendBufferIndex++] = (time[i] / 10) + '0';
+    }
+  } else {
+    ok = 0;
+  }
+
+  if (ok) {
+    receiveBuffer[13] = ' ';
+    receiveBuffer[14] = 'O';
+    receiveBuffer[15] = 'K';
+  } else {
+    receiveBuffer[13] = 'E';
+    receiveBuffer[14] = 'R';
+    receiveBuffer[15] = 'R';
+  }
+
+  historyWrite(receiveBuffer);
+
+  for (i = 0; i < receiveBufferIndex; i++) {
+    receiveBuffer[i] = ' ';
+  }
+  receiveBufferIndex = 0;
+}
+
+void handleSend() {
+  if (isSending || sendBufferIndex < 1) {
+    return;
+  }
+  isSending = 1;
+  SBUF = sendBuffer[--sendBufferIndex];
+}
+
+void main() {
+  initTimer0();
+  initTimerDisplay();
+  initMultiplexKeyboard();
+  initLCD();
+  initCommands();
+
+  while (1) {
+    handleMultiplexKeyboard();
+
+    readMatrixKeyboard();
+    handleMatrixKeyboard();
+
+    handleCommands();
+    handleSend();
+
+    if (!hasSecondPassed) {
+      continue;
+    }
+    hasSecondPassed = 0;
+    counter -= TIMER_REFRESH_RATE_0;
+
+    P1_5 = !P1_5;
+
+    prevCommandInterruptedAt = commandInterruptedAt;
+
+    if (!isTimerDisplayEdited) {
+      addTime(0, 0, 1);
+    }
+  }
+}
